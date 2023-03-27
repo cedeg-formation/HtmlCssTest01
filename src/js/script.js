0b42f99debfe1a058e035f9c05c913f2287d62c7
Julien Broens <julienbroens@gmail.com>
2023-03-27 15:21:09 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+const imageText = document.getElementById("imageText");
+const textVanilla = imageText.innerText;
+let count = 0;
+document.addEventListener(
+    "click",
+    (ev) => {
+        imageText.style.transform = `translateY(${ev.clientY - 25}px)`;
+        imageText.style.transform += `translateX(${ev.clientX - 25}px)`;
+
+    },
+    false
+);
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+}
+
+const changeText = (element) => {
+    if (count > 5) {
+        element.innerText = textVanilla;
+        count = 0;
+        return
+    }
+    const randomInt = getRandomInt(element.innerText.length)
+    element.innerText = element.innerText.replace(element.innerText[randomInt], '00');
+    count++;
+}
+setInterval(changeText, 800, imageText);
+
+
